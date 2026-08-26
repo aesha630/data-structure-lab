6019a27fb337e01792404075744b6ca194f09d48
aesha630 <aeshakhanapara2208@gmail.com>
2026-08-26 13:12:00 +0530

    Implement book ID and title input in library1.cpp
    Fixed input handling for book titles and IDs.

---
+++ library1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main()
+{
+  int id1,id2,id3;
+string title1,title2,title3;
+//BOOK1
+cout<<"Enter Book 1 ID:";
+cin>>id1;
+cin.ignor();
+cout<<"Ente Book 1 title:";
+getline cin,title1;
+cout<<"Enter Book 2 ID:"
+cin>>id2;
+cin.ignor();
+cout<<"Enter Book 2 title;";
+getline cin,title2;
+cout<<"Enter Book 3 ID:";
+cin>>id3;
+cin.ignor();
+cout<<"Enter Book 3 title :";
+getline cin,title3;
+cout<<"\n====Book library====";
+cout<<"\n..Enter id"<<id1";
+cout<<"\n..Enter title"<<title1;
+cout<<"\n..Enter id"<<id2";
+cout<<"\n..Enter title"<<title2;
+cout<<"\n..Enter id"<<id3";
+cout<<"\n..Enter title"<<title3;
+cout<<"\n..Enter id"<<id3";
+
+return 0;
+}
